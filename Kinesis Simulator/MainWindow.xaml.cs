fd715eaa42a52bffded75bb77075ea0097afe3df
Achal Aggarwal <aggarwal.achal@gmail.com>
2012-02-21 16:18:22 +0530

    Initial Commit

---
+++ Kinesis Simulator/MainWindow.xaml.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace Kinesis_Simulator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public static Server server;
+        public MainWindow()
+        {
+            InitializeComponent();
+            server = new Server();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Start_Server.IsEnabled = true;
+            Stop_Server.IsEnabled = false;
+        }
+
+        private void Window_Unloaded(object sender, RoutedEventArgs e)
+        {
+            if (server != null)
+            {
+                server.Stop();
+            }
+        }
+
+        private void Start_Server_Click(object sender, RoutedEventArgs e)
+        {
+            server.Start();
+            Start_Server.IsEnabled = false;
+            Stop_Server.IsEnabled = true;
+        }
+
+        private void Stop_Server_Click(object sender, RoutedEventArgs e)
+        {
+            if (server != null)
+            {
+                server.Stop();
+                Start_Server.IsEnabled = true;
+                Stop_Server.IsEnabled = false;
+            }
+        }
+
+        private void Speech_Send_Click(object sender, RoutedEventArgs e)
+        {
+            server.SendToAll(String.Format("{{\"gestures\":[{{\"type\":4,\"command\":\"{0}\"}}],\"cursor\":{{\"x\":50,\"y\":50,\"z\":50}}}}", textBox1.Text));
+        }
+
+
+        private void Left_SwipeLeft_Click(object sender, RoutedEventArgs e)
+        {
+            sendGesture(0, 8, 0);
+        }
+
+        private void Left_SwipeRight_Click(object sender, RoutedEventArgs e)
+        {
+            sendGesture(0, 8, 1);
+        }
+
+        private void Left_SwipeUp_Click(object sender, RoutedEventArgs e)
+        {
+            sendGesture(0, 8, 2);
+        }
+
+        private void Left_SwipeDown_Click(object sender, RoutedEventArgs e)
+        {
+            sendGesture(0, 8, 3);
+        }
+
+        private void Right_SwipeLeft_Click(object sender, RoutedEventArgs e)
+        {
+            sendGesture(0, 0, 0);
+        }
+
+        private void Right_SwipeRight_Click(object sender, RoutedEventArgs e)
+        {
+            sendGesture(0, 0, 1);
+        }
+
+        private void Right_SwipeUp_Click(object sender, RoutedEventArgs e)
+        {
+            sendGesture(0, 0, 2);
+        }
+
+        private void Right_SwipeDown_Click(object sender, RoutedEventArgs e)
+        {
+            sendGesture(0, 0, 3);
+        }
+
+        private void ellipse1_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Point mousePos = Mouse.GetPosition(canvas1);
+            Trace.WriteLine(mousePos.ToString());
+            Canvas.SetLeft(ellipse1, mousePos.X);
+            Canvas.SetTop(ellipse1, mousePos.Y);
+        }
+
+        bool mouseClicked = false;
+
+        private void canvas1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseClicked == false)
+                return;
+
+            Point mousePos = Mouse.GetPosition(canvas1);
+            if (mousePos.X > 200 || mousePos.Y > 200 || mousePos.X < 0 || mousePos.Y < 0)
+            {
+                mouseClicked = false;
+                return;
+            }      
+            Canvas.SetLeft(ellipse1, mousePos.X-10);
+            Canvas.SetTop(ellipse1, mousePos.Y-10);
+            server.SendToAll(String.Format("{{\"cursor\":{{\"x\":{0},\"y\":{1},\"z\":{2}}}}}", (mousePos.X / 2).ToString(), (mousePos.Y / 2).ToString(), depthValue.Content));
+        }
+
+        private void canvas1_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            mouseClicked = true;
+        }
+
+        private void canvas1_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            mouseClicked = false;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (server != null)
+            {
+                server.Stop();
+            }
+        }
+
+        private void Left_holdLeft_Click(object sender, RoutedEventArgs e)
+        {
+            this.sendGesture(2, 8, 0);
+        }
+
+        private void sendGesture(int type, int joint, int direction)
+        {
+            server.SendToAll(String.Format("{{\"gestures\":[{{\"type\":{0},\"joints\":[{1}],\"direction\":{2},\"origin\":{{\"x\":{3},\"y\":{4},\"z\":{5}}}}}],\"cursor\":{{\"x\":50,\"y\":50,\"z\":50}}}}", type, joint, direction, originalX.Text, originalY.Text, originalZ.Text));
+        }
+
+        private void Left_holdUp_Click(object sender, RoutedEventArgs e)
+        {
+            sendGesture(2, 8, 2);
+        }
+
+        private void Left_holdFront_Click(object sender, RoutedEventArgs e)
+        {
+         
+            sendGesture(2, 8, 4);
+        }
+
+        private void Right_holdRight_Click(object sender, RoutedEventArgs e)
+        {
+            server.SendToAll("{\"gestures\":[{\"type\":2,\"joints\":[0],\"direction\":1}],\"cursor\":{\"x\":50,\"y\":50,\"z\":50}}");
+        }
+
+        private void Right_holdUp_Click(object sender, RoutedEventArgs e)
+        {
+            server.SendToAll("{\"gestures\":[{\"type\":2,\"joints\":[0],\"direction\":2}],\"cursor\":{\"x\":50,\"y\":50,\"z\":50}}");
+        }
+
+        private void Right_holdFront_Click(object sender, RoutedEventArgs e)
+        {
+            server.SendToAll("{\"gestures\":[{\"type\":2,\"joints\":[0],\"direction\":4}],\"cursor\":{\"x\":50,\"y\":50,\"z\":50}}");
+        }
+
+        private void leanLeft_Click(object sender, RoutedEventArgs e)
+        {
+            server.SendToAll("{\"gestures\":[{\"type\":1,\"subtype\":0,\"direction\":0}],\"cursor\":{\"x\":50,\"y\":50,\"z\":50}}");
+        }
+
+        private void leanRight_Click(object sender, RoutedEventArgs e)
+        {
+            server.SendToAll("{\"gestures\":[{\"type\":1,\"subtype\":0,\"direction\":1}],\"cursor\":{\"x\":50,\"y\":50,\"z\":50}}");
+        }
+
+        private void leanFront_Click(object sender, RoutedEventArgs e)
+        {
+            server.SendToAll("{\"gestures\":[{\"type\":1,\"subtype\":0,\"direction\":4}],\"cursor\":{\"x\":50,\"y\":50,\"z\":50}}");
+        }
+
+        private void Jump_Click(object sender, RoutedEventArgs e)
+        {
+            server.SendToAll("{\"gestures\":[{\"type\":1,\"subtype\":0,\"direction\":2}],\"cursor\":{\"x\":50,\"y\":50,\"z\":50}}");
+        }
+
+        private void Crouch_Click(object sender, RoutedEventArgs e)
+        {
+            server.SendToAll("{\"gestures\":[{\"type\":1,\"subtype\":0,\"direction\":3}],\"cursor\":{\"x\":50,\"y\":50,\"z\":50}}");
+        }
+
+        private void turnLeft_Click(object sender, RoutedEventArgs e)
+        {
+            server.SendToAll("{\"gestures\":[{\"type\":1,\"subtype\":1,\"direction\":0}],\"cursor\":{\"x\":50,\"y\":50,\"z\":50}}");
+        }
+
+        private void turnRight_Click(object sender, RoutedEventArgs e)
+        {
+            server.SendToAll("{\"gestures\":[{\"type\":1,\"subtype\":1,\"direction\":1}],\"cursor\":{\"x\":50,\"y\":50,\"z\":50}}");
+        }
+
+        private void slider1_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            depthValue.Content = slider1.Value.ToString();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            server.SendToAll("{\"Kinect\":\"Connected\"}");
+            button2.IsEnabled = true;
+            button1.IsEnabled = false;
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            server.SendToAll("{\"Kinect\":\"Disconnected\"}");
+            button2.IsEnabled = false;
+            button1.IsEnabled = true;
+        }
+    }
+}
---
+++ Kinesis Simulator/Server.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.SocketBase;
+using SuperSocket.SocketBase.Config;
+using SuperSocket.SocketEngine;
+using SuperWebSocket;
+using System.Timers;
+using System.Diagnostics; 
+
+namespace Kinesis_Simulator
+{
+    public class Server
+    {
+        private List<WebSocketSession> m_Sessions = new List<WebSocketSession>();
+        private object m_SessionSyncRoot = new object();
+        private object m_SecureSessionSyncRoot = new object();
+        private Timer m_SecureSocketPushTime;
+
+        private static WebSocketServer socketServer;
+
+        public Server()
+        {
+            socketServer = new WebSocketServer();
+            socketServer.Setup(new RootConfig(),
+                new ServerConfig
+                {
+                    Name = "SuperWebSocket",
+                    Ip = "Any",
+                    Port = 2011,
+                    Mode = SocketMode.Sync
+                }, SocketServerFactory.Instance);
+        }
+
+        internal void Start()
+        {
+            socketServer.NewMessageReceived += new SessionEventHandler<WebSocketSession, string>(socketServer_NewMessageReceived);
+            socketServer.NewSessionConnected += new SessionEventHandler<WebSocketSession>(socketServer_NewSessionConnected);
+            socketServer.SessionClosed += new SessionEventHandler<WebSocketSession, CloseReason>(socketServer_SessionClosed);
+            socketServer.Start();
+        }
+
+        internal void Stop()
+        {
+            if (socketServer.IsRunning)
+            {
+                socketServer.NewMessageReceived -= socketServer_NewMessageReceived;
+                socketServer.NewSessionConnected -= socketServer_NewSessionConnected;
+                socketServer.SessionClosed -= socketServer_SessionClosed;
+                socketServer.Stop();
+            }
+        }
+
+        void socketServer_NewMessageReceived(WebSocketSession session, string e)
+        {
+            SendToAll(session.Cookies["name"] + ": " + e);
+        }
+
+        void socketServer_NewSessionConnected(WebSocketSession session)
+        {
+            lock (m_SessionSyncRoot)
+                m_Sessions.Add(session);
+        }
+
+        void socketServer_SessionClosed(WebSocketSession session, CloseReason reason)
+        {
+            lock (m_SessionSyncRoot)
+                m_Sessions.Remove(session);
+
+            if (reason == CloseReason.ServerShutdown)
+                return;
+        }
+
+        public void SendToAll(string message)
+        {
+            lock (m_SessionSyncRoot)
+            {
+                foreach (var s in m_Sessions)
+                {
+                    s.SendResponse(message);
+                }
+            }
+        }
+    }
+}
